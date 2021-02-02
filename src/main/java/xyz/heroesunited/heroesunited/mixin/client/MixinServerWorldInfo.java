95d580aba173401d9d6d2020989df8cf0438b151
Chappie <53862142+ChappiePie@users.noreply.github.com>
2021-02-02 17:44:23 +0200

    added MixinServerWorldInfo, deleted MixinMinecraft

---
+++ src/main/java/xyz/heroesunited/heroesunited/mixin/client/MixinServerWorldInfo.java
@@ -0,0 +1,17 @@
+package xyz.heroesunited.heroesunited.mixin.client;
+
+import com.mojang.serialization.Lifecycle;
+import net.minecraft.world.storage.ServerWorldInfo;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ServerWorldInfo.class)
+public class MixinServerWorldInfo {
+
+    @Inject(at = @At("HEAD"), method = "Lnet/minecraft/world/storage/ServerWorldInfo;getLifecycle()Lcom/mojang/serialization/Lifecycle;", cancellable = true)
+    private void getLifecycle(CallbackInfoReturnable<Lifecycle> cir) {
+        cir.setReturnValue(Lifecycle.stable());
+    }
+}
