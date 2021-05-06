8363af4aff96544b9246fdebc4cf4a9c05155839
grillo78 <sr.grillo79@gmail.com>
2021-05-07 01:52:54 +0200

    added starry sky

--- src/main/java/xyz/heroesunited/heroesunited/client/SpaceSkyRenderHandler.java
+++ src/main/java/xyz/heroesunited/heroesunited/client/SpaceSkyRenderHandler.java
@@ -1,14 +1,72 @@
 package xyz.heroesunited.heroesunited.client;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.client.renderer.vertex.VertexBuffer;
+import net.minecraft.client.renderer.vertex.VertexFormat;
 import net.minecraft.client.world.ClientWorld;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.vector.Matrix4f;
+import net.minecraft.util.math.vector.Vector3f;
 import net.minecraftforge.client.ISkyRenderHandler;
 
+import javax.annotation.Nullable;
+import java.util.Random;
+
 public class SpaceSkyRenderHandler implements ISkyRenderHandler {
+    
+    private static final ResourceLocation SPACE_SKY_LOCATION = new ResourceLocation("textures/entity/end_portal.png");
 
     @Override
     public void render(int ticks, float partialTicks, MatrixStack matrixStack, ClientWorld world, Minecraft mc) {
+        RenderSystem.disableAlphaTest();
+        RenderSystem.enableBlend();
+        RenderSystem.defaultBlendFunc();
+        RenderSystem.depthMask(false);
+        RenderSystem.scalef(3, 3, 1);
+        Minecraft.getInstance().textureManager.bind(SPACE_SKY_LOCATION);
+        Tessellator tessellator = Tessellator.getInstance();
+        BufferBuilder bufferbuilder = tessellator.getBuilder();
 
+        for(int i = 0; i < 6; ++i) {
+            matrixStack.pushPose();
+            if (i == 1) {
+                matrixStack.mulPose(Vector3f.XP.rotationDegrees(90.0F));
+            }
+
+            if (i == 2) {
+                matrixStack.mulPose(Vector3f.XP.rotationDegrees(-90.0F));
+            }
+
+            if (i == 3) {
+                matrixStack.mulPose(Vector3f.XP.rotationDegrees(180.0F));
+            }
+
+            if (i == 4) {
+                matrixStack.mulPose(Vector3f.ZP.rotationDegrees(90.0F));
+            }
+
+            if (i == 5) {
+                matrixStack.mulPose(Vector3f.ZP.rotationDegrees(-90.0F));
+            }
+
+            Matrix4f matrix4f = matrixStack.last().pose();
+            bufferbuilder.begin(7, DefaultVertexFormats.POSITION_TEX_COLOR);
+            bufferbuilder.vertex(matrix4f, -100.0F, -100.0F, -100.0F).uv(0.0F, 0.0F).color(40, 40, 40, 255).endVertex();
+            bufferbuilder.vertex(matrix4f, -100.0F, -100.0F, 100.0F).uv(0.0F, 16.0F).color(40, 40, 40, 255).endVertex();
+            bufferbuilder.vertex(matrix4f, 100.0F, -100.0F, 100.0F).uv(16.0F, 16.0F).color(40, 40, 40, 255).endVertex();
+            bufferbuilder.vertex(matrix4f, 100.0F, -100.0F, -100.0F).uv(16.0F, 0.0F).color(40, 40, 40, 255).endVertex();
+            tessellator.end();
+            matrixStack.popPose();
+        }
+
+        RenderSystem.depthMask(true);
+        RenderSystem.enableTexture();
+        RenderSystem.disableBlend();
+        RenderSystem.enableAlphaTest();
     }
 }
