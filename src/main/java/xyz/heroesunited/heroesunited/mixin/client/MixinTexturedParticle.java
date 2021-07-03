0a9cba8509ef88c0a7981d1fe8212e107496f564
Chappie <53862142+ChappiePie@users.noreply.github.com>
2021-07-03 22:43:52 +0300

    Yeah particles

--- src/main/java/xyz/heroesunited/heroesunited/mixin/client/MixinTexturedParticle.java
+++ src/main/java/xyz/heroesunited/heroesunited/mixin/client/MixinTexturedParticle.java
@@ -2,6 +2,7 @@ package xyz.heroesunited.heroesunited.mixin.client;
 
 import net.minecraft.client.particle.TexturedParticle;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import xyz.heroesunited.heroesunited.HeroesUnited;
@@ -9,11 +10,13 @@ import xyz.heroesunited.heroesunited.HeroesUnited;
 @Mixin(TexturedParticle.class)
 public abstract class MixinTexturedParticle {
 
+    @Shadow public abstract float getQuadSize(float p_217561_1_);
+
     @Redirect(method = "render(Lcom/mojang/blaze3d/vertex/IVertexBuilder;Lnet/minecraft/client/renderer/ActiveRenderInfo;F)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/particle/TexturedParticle;getQuadSize(F)F"))
-    private float injected(TexturedParticle texturedParticle, float quad) {
+    private float changeQuadSize(TexturedParticle texturedParticle, float partialTicks) {
         if (((MixinIParticle) this).getLevel().dimension().equals(HeroesUnited.SPACE)) {
-            return quad*0.01F;
+            return this.getQuadSize(partialTicks) *0.01F;
         }
-        return quad;
+        return this.getQuadSize(partialTicks);
     }
 }
